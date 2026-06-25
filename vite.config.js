8cba1ba300ecb152b34deb91b0fc8374876a8391
ChenMike <mike@guidercard.com>
2026-06-25 11:28:49 +0800

    打包測試

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/gaider-simplified-version/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
